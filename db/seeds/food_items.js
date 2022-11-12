bebe0deabef5359756b18cdc80b930963b200a14
Ed Cuevas <edjanncuevas@icloud.com>
2022-11-13 02:22:40 +0900

    Feat: Change date to timestamp

--- db/seeds/food_items.js
+++ db/seeds/food_items.js
@@ -6,8 +6,8 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('pantry').del()
   await knex('pantry').insert([
-    {name: 'Bananas', date: '2022-11-04T11:24:32.510Z'},
-    {name: 'Eggs', date: '2022-11-10T12:06:42.012Z'},
-    {name: 'Milk', date: '2022-11-06T15:01:52.037Z'}
+    {name: 'Bananas', timestamp: '2022-11-04T11:24:32.510Z'},
+    {name: 'Eggs', timestamp: '2022-11-10T12:06:42.012Z'},
+    {name: 'Milk', timestamp: '2022-11-06T15:01:52.037Z'}
   ]);
 };
