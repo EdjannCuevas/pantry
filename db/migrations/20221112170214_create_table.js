ccf9aa48f070ad1dca17e67a69255c16e61fe3a6
Ed Cuevas <edjanncuevas@icloud.com>
2022-11-13 02:22:20 +0900

    Feat: Add timestamp column

---
+++ db/migrations/20221112170214_create_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+    return await knex.schema.createTable('pantry', (table) => {
+        table.increments('id').primary(),
+        table.string('name', 255).notNullable(),
+        table.timestamp('timestamp').notNullable();
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+    return await knex.schema.dropTable('pantry')
+};
