a45e6cf3857ba2050c310f584fcdb32f35a46591
Ed Cuevas <edjanncuevas@icloud.com>
2022-11-12 21:35:07 +0900

    Feat: Update knex file

--- db/knex.js
+++ db/knex.js
@@ -1,6 +1,6 @@
 require('dotenv').config;
 const knex = require('knex');
 const config = require('../knexfile');
-const env = NODE_ENV ? 'production' : 'development';
+const env = process.env.NODE_ENV ? 'production' : 'development';
 
 module.exports = knex(config[env]);
