93ec402f2a66cf343d3858d47a8293abe3276543
Ed Cuevas <edjanncuevas@icloud.com>
2022-11-14 01:48:52 +0900

    Feat: Add Navbar css

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,7 +1,9 @@
 import React from "react";
-
+import stirfry from '../Styles/stirfry.gif';
+import '../Styles/NavBar.css'
 const Navbar = () => {
-    return <div>
+    return <div className="navbar__container">
+        <img src={stirfry}/>
         <h1>Navbar</h1>
     </div>
 };
