974df75902d191d82f0cdc693e998e9bb75c3185
Ed Cuevas <edjanncuevas@icloud.com>
2022-11-13 16:36:46 +0900

    Feat: Set up new endpoints for ingredients table

--- server/server.js
+++ server/server.js
@@ -73,10 +73,63 @@ function setUpServer () {
         };
     });
 
-    app.get('/test', async (req, res) => {
+    app.get('/ingredients', async (req, res) => {
         try {
-            await db('pantry').select('*');
-            res.status(200).send("🐱‍🚀🐱‍👓🐱🐱‍🐉");
+            const ingredients = await db('ingredients')
+                .select('*');
+            res.status(201).send(ingredients);
+
+        } catch (error) {
+          console.log(error);  
+        };
+    });
+
+    app.get('/ingredients/:id', async (req, res) => {
+        try {
+            const { id } = req.params;
+            const item = await db('ingredients')
+                .select('*')
+                .where({ pantry_id : id });
+            res.status(201).send(item);
+
+        } catch (error) {
+            console.log(error);
+        };
+    });
+
+    app.post('/ingredients', async (req, res) => {
+        try {
+            await db('ingredients')
+                .insert(req.body);
+            res.status(201).send('Success!');
+
+        } catch (error) {
+            console.log(error);
+        };
+    });
+
+    app.put('/ingredients/:id', async (req, res) => {
+        try {
+            const { id } = req.params;
+            
+            await db('ingredients')
+                .where({ pantry_id : id })
+                .update(req.body, ['name', 'timestamp']);
+            res.status(201).send('Updated!');
+
+        } catch (error) {
+            console.log(error);
+        };
+    });
+
+    app.delete('/ingredients/:id', async (req, res) => {
+        try {
+            const { id } = req.params;
+            
+            await db('ingredients')
+                .where({ pantry_id: id })
+                .delete();
+            res.status(201).send('Item was deleted!');
 
         } catch (error) {
             console.log(error);
