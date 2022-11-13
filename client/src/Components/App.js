a6eb1ee2d9bf434ab7c95ec9ff92a2261ea71c6e
Ed Cuevas <edjanncuevas@icloud.com>
2022-11-14 01:50:37 +0900

    Feat: Add a Recipes component

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,12 +1,16 @@
 import '../Styles/App.css';
+import React from 'react';
 import NavBar from './Navbar';
 import Input from './Input';
+import Recipes from './Recipes.js';
 
 function App() {
+  
   return (
     <div className="App">
       <NavBar/>
       <Input/>
+      <Recipes/>
     </div>
   );
 }
