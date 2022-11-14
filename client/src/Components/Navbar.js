7aa1d051ab3f57c09a6f60f464387004c423191c
Ed Cuevas <edjanncuevas@icloud.com>
2022-11-14 14:39:32 +0900

    Feat: Add more CSS styling

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -3,8 +3,8 @@ import React from "react";
 import stirfry from '../Styles/stirfry.gif';
 const Navbar = () => {
     return <div className="navbar__container">
-        <img src={stirfry}/>
         <h1>PANTRY</h1>
+        <img alt='stirfry_logo' src={stirfry}/>
     </div>
 };
 
