1c4a77df3e28ce175a663b5e74f81242f63d7586
Ed Cuevas <edjanncuevas@icloud.com>
2022-11-14 20:18:44 +0900

    Feat: Remove console logs

--- client/src/Components/Recipes.js
+++ client/src/Components/Recipes.js
@@ -10,11 +10,9 @@ const Recipes = () => {
     },[]);
 
     async function getRecipes () {
-        console.log(1);
         const app_id = '59482d1c';
         const app_key = '6708bc487e05e9385e5d27f95ed728f3';
         const fetchedIngredientsList = await axios.get('/ingredients');
-        console.log(2);
         const ingredientsList = fetchedIngredientsList.data.map((ingredient) => {
             return ingredient.name 
         });
@@ -23,7 +21,6 @@ const Recipes = () => {
         });
 
         const spacedIngredients = ingredientsList.join('%20and%20').toLowerCase();
-        console.log(3);
         const fetchedRecipeList = await axios.get(`https://api.edamam.com/api/recipes/v2?type=public&q=${spacedIngredients}&app_id=${app_id}&app_key=${app_key}`);
         const recipeList = fetchedRecipeList.data.hits.map((item) => {
             return  <p>{ item.recipe.label }</p>
@@ -35,6 +32,7 @@ const Recipes = () => {
 
     return <div>
         <h1>Recipes</h1>
+        { ingredients }
         { recipes }
     </div>
 };
