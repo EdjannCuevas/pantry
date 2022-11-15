3dd5c4861cef20008d8501927df67a11fe08515a
Ed Cuevas <edjanncuevas@icloud.com>
2022-11-15 13:22:41 +0900

    Feat: Change UserInput name

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,7 +1,7 @@
 import '../Styles/App.css';
 import React from 'react';
 import NavBar from './Navbar';
-import Input from './Input';
+import UserInput from './UserInput';
 import Recipes from './Recipes.js';
 import { Routes, Route } from 'react-router-dom';
 
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <NavBar/>
       <Routes>
-        <Route path='/' element={ <Input/> } />
+        <Route path='/' element={ <UserInput/> } />
         <Route path='/recipes' element={ <Recipes/> } />
       </Routes>
     </div>
