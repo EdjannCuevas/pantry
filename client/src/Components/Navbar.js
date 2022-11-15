47664c6b4b667bfe6a3ce1e1c6de13a23efe5ce5
Ed Cuevas <edjanncuevas@icloud.com>
2022-11-16 01:47:45 +0900

    Feat: Add onClick  in Navbar title to navigate to home

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,9 +1,19 @@
 import '../Styles/Navbar.css'
 import React from "react";
 import stirfry from '../Styles/stirfry.gif';
+import { useNavigate } from 'react-router-dom';
 const Navbar = () => {
+    const navigate = useNavigate();
+
     return <div className="navbar__container">
-        <h1 id='app__name' >PANTRY</h1>
+        <h1
+            id='app__name'
+            style={{cursor:'pointer'}}
+            onClick={(e) => {
+                e.preventDefault();
+                navigate('/');
+            }
+        }>PANTRY</h1>
         <img id='stirfry__gif' alt='stirfry_gif' src={stirfry}/>
     </div>
 };
