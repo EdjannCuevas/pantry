527cdc1c982386676ac6f159b0f45be560d3f6c6
Ed Cuevas <edjanncuevas@icloud.com>
2022-12-29 15:09:30 +0900

    style: stylize sign in and aign up forms

--- client/src/Components/auth/SignIn.jsx
+++ client/src/Components/auth/SignIn.jsx
@@ -1,4 +1,4 @@
-import { Button } from "@mui/material";
+import { Alert, Button, Card, CardHeader, Container, FormControl, FormControlLabel, Input, InputLabel, TextField } from "@mui/material";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -9,43 +9,55 @@ const SignIn = ({ setUid }) => {
     const navigate = useNavigate();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [isNotUser, setIsNotUser] = useState(false);
 
     const signIn = (e) => {
         e.preventDefault();
         signInWithEmailAndPassword(auth, email, password)
         .then((userCredentials) => {
             setUid(userCredentials.user.uid);
-            navigate('/home')
+            setIsNotUser(false);
+            navigate('/home');
         })
         .catch((err) => {
-            console.log(err)
+            setIsNotUser(true);
+            console.log(err);
         })
     }
 
     return (
         <div className="sign__in__container">
-            <form onSubmit={signIn}>
-                <h1>Log in</h1>
-                <input
-                    type='text'
-                    placeholder='Enter your email'
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                ></input>
-                <input 
-                    type='password'
-                    placeholder='Enter your password'
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                ></input>
-                <button type="submit">Log in</button>
-            </form>
-            <p>Don't have and account?</p>
-            <Button onClick={(e) => {
-                e.preventDefault();
-                navigate('/signUp');
+            <Card raised='true' className='sign__in__form'>
+                <CardHeader title='Log in'/>
+                {
+                    (isNotUser) ? (
+                        <Alert variant='outlined' severity="error">Incorrect email or password!</Alert>
+                    ) : (<p> </p>)
                 }
-            }>Sign Up</Button>
+                <FormControl>
+                    <TextField
+                        variant="filled"
+                        type='text'
+                        label='Email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    ></TextField>
+                    <TextField 
+                        variant="filled"
+                        type='password'
+                        label='Password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    ></TextField>
+                    <Button variant='contained' type="submit" onClick={signIn} >Log in</Button>
+                </FormControl>
+                <p>Don't have and account?</p>
+                <Button onClick={(e) => {
+                    e.preventDefault();
+                    navigate('/signup');
+                    }
+                }>Sign Up</Button>
+            </Card>
         </div>
     )
 }
--- client/src/Components/auth/SignUp.jsx
+++ client/src/Components/auth/SignUp.jsx
@@ -1,41 +1,66 @@
+import { Alert, Button, Card, CardHeader, FormControl, TextField } from "@mui/material";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { auth } from "../../firebase";
 import '../../Styles/SignIn.css';
 
 const SignUp = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [isRegistered, setIsRegistered] = useState(false);
+    const [isValid, setIsValid] = useState(true);
+    const navigate = useNavigate();
 
     const signUp = (e) => {
         e.preventDefault();
         createUserWithEmailAndPassword(auth, email, password)
         .then((userCredentials) => {
+            setIsValid(true);
+            setIsRegistered(true);
             console.log(userCredentials)
         })
         .catch((err) => {
+            setIsValid(false);
             console.log(err)
         })
     }
 
     return (
         <div className="sign__in__container">
-            <form onSubmit={signUp}>
-                <h1>Create Account</h1>
-                <input
-                    type='text'
-                    placeholder='Enter your email'
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                ></input>
-                <input 
-                    type='password'
-                    placeholder='Enter your password'
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                ></input>
-                <button type="submit">Sign Up</button>
-            </form>
+            <Card raised='true' className="sign__in__form">
+                <CardHeader title='Sign up'/>
+                {
+                    (isValid && isRegistered) ? (
+                        <Alert variant="outlined" severity="success">Success! You can can now sign in.</Alert>
+                    ) : (!isValid) ? (
+                        <Alert variant="outlined" severity="error">Invalid email or password.</Alert>
+                    ) : (<p>    </p>)
+                }
+                <FormControl>
+                    <TextField
+                        variant="filled"
+                        type='text'
+                        label='Enter your email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    ></TextField>
+                    <TextField 
+                        variant='filled'
+                        type='password'
+                        label='Enter your password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    ></TextField>
+                    <Button variant="contained" type="submit" onClick={signUp}>Sign up</Button>
+                </FormControl>
+                    <p>Already have an account?</p>
+                    <Button type="submit" onClick={(e) => {
+                        e.preventDefault();
+                        navigate('/');
+                        }
+                    }>Sign in</Button>
+            </Card>
         </div>
     )
 }
