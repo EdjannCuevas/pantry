0adecf0133c2987241ed940742e6de464480dbef
Ed Cuevas <edjanncuevas@icloud.com>
2023-03-12 18:43:14 +0900

    fix: database config variable

--- server/index.js
+++ server/index.js
@@ -3,9 +3,6 @@ const app = server();
 const PORT = process.env.PORT || 4000;
 
 (async () => {
-    console.log(process.env.DB_URL_PROD);
-    console.log(process.env.NODE_ENV);
-    console.log(process.env.PORT);
     
     try {
         app.listen(PORT, () => {
