d9eb5d5c9f8ff24999536b3343df65af71e01da5
Ed Cuevas <edjanncuevas@icloud.com>
2023-03-23 22:15:50 +0900

    style: add padding and margin to recipes

--- client/src/Components/Recipes.tsx
+++ client/src/Components/Recipes.tsx
@@ -17,6 +17,92 @@ const Recipes: React.FC<RecipesProps> = ({ search }) => {
     const [ingredients, setIngredients] = useState([]);
     
     useEffect(() => {
+        async function getRecipes () {
+            const app_id = '59482d1c';
+            const app_key = '6708bc487e05e9385e5d27f95ed728f3';
+            const fetchedIngredientsList = await axios.get(`/ingredients/${getUid()}`);
+            const ingredientsList = fetchedIngredientsList.data.map((ingredient: IngredientsObj) => {
+                return ingredient.name 
+            });
+            setIngredients(ingredientsList.map((ingredient: string) => {
+                return <Button
+                        disabled
+                        color='success'
+                        variant='contained'
+                        size='small'
+                        >x { ingredient }</Button>
+            }));
+    
+            const spacedIngredients = (search ? search : ingredientsList).join('%20and%20').toLowerCase();
+            const fetchedRecipeList = await axios.get(`https://api.edamam.com/api/recipes/v2?type=public&q=${spacedIngredients}&app_id=${app_id}&app_key=${app_key}`);
+            const recipeList = fetchedRecipeList.data.hits.map((item: ResponseObj) => {
+                const recipe = item.recipe;
+    
+                const name = recipe.label;
+                const image = recipe.image;
+                const recipeIngredients = recipe.ingredientLines;
+                const cookTime = recipe.totalTime;
+                const servings = recipe.yield;
+                const calories = Math.floor(recipe.calories/servings);
+                const source = recipe.url;
+    
+                return  <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                <TableCell>
+                    <Card>
+                        <div className='recipe__container'>
+                            <div className='food__image__container'>
+
+                            <img className='food__image'  alt='dish' src={ image }/>
+                            </div>
+                            <div className='raw__data__container'>
+                                <h2 className='recipe__name'>{ name }</h2>
+                                <div className='ingredients__container'>
+                                    <p>
+                                        <b>Ingredients:</b>
+                                        { recipeIngredients.join(', ') }
+                                    </p>
+                                </div>
+                                <div className='icons__container'>
+                                    <Card className='icon__data'>
+                                        <HourglassTop sx={{ fontSize: 45 }}/>
+                                        <b>Cooking Time: { cookTime } min/s</b>
+                                    </Card>
+                                    <Card className='icon__data'>
+                                        <Restaurant sx={{ fontSize: 45 }}/>
+                                        <b>Servings: { servings } </b>
+                                    </Card>
+                                    <Card className='icon__data'>
+                                        <Scale sx={{ fontSize: 45 }}/>
+                                        <b>Calories: { calories }</b>
+                                    </Card>
+                                </div>
+ 
+                            </div>
+                            <div className='recipe__buttons__container'>
+                                <Button className='view__button'
+                                    variant='contained'
+                                    color='primary'
+                                    onClick={() => openInNewTab( source )}
+                                    >Recipe
+                                </Button>
+                                <IconButton
+                                    color="primary"
+                                    aria-label="add to shopping cart"
+                                    onClick={() => {
+                                        addToGroceryList(name, calories, image, recipeIngredients, source);
+                                        navigate('/lists');
+                                        }
+                                    }>
+                                    <LocalGroceryStore color='primary' sx={{ fontSize: 45 }}/>
+                                </IconButton>
+                            </div>
+                        </div>
+                    </Card>
+                </TableCell>
+            </TableRow>
+            });
+            setRecipes(recipeList);
+        };
         getRecipes();
     },[]);
 
@@ -39,85 +125,6 @@ const Recipes: React.FC<RecipesProps> = ({ search }) => {
         }
     };
 
-    async function getRecipes () {
-        const app_id = '59482d1c';
-        const app_key = '6708bc487e05e9385e5d27f95ed728f3';
-        const fetchedIngredientsList = await axios.get(`/ingredients/${getUid()}`);
-        const ingredientsList = fetchedIngredientsList.data.map((ingredient: IngredientsObj) => {
-            return ingredient.name 
-        });
-        setIngredients(ingredientsList.map((ingredient: string) => {
-            return <Button
-                    disabled
-                    color='success'
-                    variant='contained'
-                    size='small'
-                    >x { ingredient }</Button>
-        }));
-
-        const spacedIngredients = (search ? search : ingredientsList).join('%20and%20').toLowerCase();
-        const fetchedRecipeList = await axios.get(`https://api.edamam.com/api/recipes/v2?type=public&q=${spacedIngredients}&app_id=${app_id}&app_key=${app_key}`);
-        const recipeList = fetchedRecipeList.data.hits.map((item: ResponseObj) => {
-            const recipe = item.recipe;
-
-            const name = recipe.label;
-            const image = recipe.image;
-            const recipeIngredients = recipe.ingredientLines;
-            const cookTime = recipe.totalTime;
-            const servings = recipe.yield;
-            const calories = Math.floor(recipe.calories/servings);
-            const source = recipe.url;
-
-            return  <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-            <TableCell className='table__cell'>
-                <Card className='recipe__data__container'>
-                    <div className='recipe__info'>
-                        <img className='food__image'  alt='dish' src={ image }/>
-                        <div className='raw__data__container'>
-                            <div className='raw__data'>
-                                <p className='recipe__name'>{ name }</p>
-                                <div className='icons__container'>
-                                    <Card className='icon__data'>
-                                        <HourglassTop sx={{ fontSize: 50 }}/>
-                                        <b>Cooking Time: { cookTime } min/s</b>
-                                    </Card>
-                                    <Card className='icon__data'>
-                                        <Restaurant sx={{ fontSize: 50 }}/>
-                                        <b>Servings: { servings }</b>
-                                    </Card>
-                                    <Card className='icon__data'>
-                                        <Scale sx={{ fontSize: 50 }}/>
-                                        <b>Calories: { calories }</b>
-                                    </Card>
-                                </div>
-                            <p><b>Ingredients:</b> { recipeIngredients.join(', ') } </p>
-                            </div> 
-                        </div>
-                        <div className='recipe__buttons__container'>
-                            <Button className='view__button'
-                                variant='contained'
-                                color='primary'
-                                onClick={() => openInNewTab(source)}
-                                >Recipe
-                            </Button>
-                            <IconButton
-                                color="primary"
-                                aria-label="add to shopping cart"
-                                onClick={() => {
-                                    addToGroceryList(name, calories, image, recipeIngredients, source);
-                                    navigate('/lists');
-                                    }
-                                }>
-                                <LocalGroceryStore color='primary' sx={{ fontSize: 40 }}/>
-                            </IconButton>
-                        </div>
-                    </div>
-                </Card>
-            </TableCell>
-        </TableRow>
-        });
-        setRecipes(recipeList);
-    };
     
 
     return <div className='recipes__page'>
